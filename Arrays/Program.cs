0f81041ebf4d6b2f3f1f3b9f33ec27d9e1eea374
Keidi Russak <keidi.russak@gmail.com>
2024-02-20 19:40:58 +0200

    String and Array

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿//massiivid - keerulisemad/rangemad andmetüübid. hiljem ei saa muuta et salvestada teatud andmeid ühes kohas. need ei pea olema numbrid, andmed jne
+//string[] fruit = { "apples", "bananas", "oranges" };//[] tähendab et ei ole primitiivne vaid on kogum
+//{} vahel peavad olema samasugused andmetüübid
+string[] fruit = new string[3];
+
+fruit[0] = "apples";
+fruit[1] = "bananas";
+fruit[2] = "oranges";
+Console.WriteLine($"{fruit[0]}, {fruit[1]}, {fruit[2]}");
+
+int fruitArrayLength = fruit.Length;// ei tea palju andmeid on
+Console.WriteLine($"There are {fruitArrayLength} in your array.");
+
+for (int i = 0; i < fruitArrayLength; i++)
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+
+foreach (string element in fruit)//iga sõne fruit massiivis
+{
+    Console.WriteLine(element);
+}
+
+
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿string hello = " we are the champions?"; // string - mitte primitiivne andmetüüp. Keerulisemad, hoitakse vahemälus eraldi kohas.
+// oluline omadus --> lenght 
+//tühiku kood --> 32. suured tähed --> 65-90. väikesed tähed 97-122.
+int stringLength  = hello.Length; // lenGTH mitte lenght
+// kaks võimalust tühiku eemaldamiseks:
+//1. string trimmedString = hello.Trim(); // () --> teen midagi. Trim --> eemaldab tühikud enne sõnet ja peale sõnet
+hello = hello.Trim();//2.
+char firstLetter = hello[0]; // string tüüpi andmed on immutable
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+
+int wordCounter = 1;
+for (int i = 0; i < hello.Length; i++)
+{
+    if (char.IsWhiteSpace(hello[i]))
+    {
+        wordCounter++;
+    }
+    //if (hello[i] == ' ')
+    //{  
+    //    wordCounter++; 
+    //}
+}
+Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
+ 
